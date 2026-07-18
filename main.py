994639790f16e51196065ac7a46e524f83836b39
Valeria-Atehortua <valeria.atehortu665@gmail.com>
2026-07-18 10:45:31 -0500

    Implementar cálculo de interés compuesto

--- main.py
+++ main.py
@@ -40,13 +40,41 @@ def interes_simple():
     
     input("\nPresione Enter para volver al menú...")
 
+def interes_compuesto():
+    print("Elegiste interés compuesto")
+
+    capital = float(input("Ingrese el capital inicial: "))
+    tasa_interes = float(input("Ingrese el porcentaje de la tasa de interés: "))
+    tiempo = float(input("Ingrese el tiempo en años: "))
+
+    print("=" * ANCHO)
+    print("DATOS INGRESADOS".center(ANCHO))
+    print("=" * ANCHO)
+    
+    print(f"Capital: ${capital:,.2f}")
+    print(f"Tasa: {tasa_interes:,.2f}%")
+    print(f"Tiempo: {tiempo} años")
+
+    tasa_interes /= 100
+    monto_final = capital * (1 + tasa_interes) ** tiempo
+    interes = monto_final - capital
+
+    print("=" * ANCHO)
+    print("RESULTADOS".center(ANCHO))
+    print("=" * ANCHO)
+    print(f"\nCapital inicial: ${capital:,.2f}")
+    print(f"Interés generado: ${interes:,.2f}")
+    print(f"Monto final: ${monto_final:,.2f}")
+    
+    input("\nPresione Enter para volver al menú...")
+
 def main():
     while True:
         opcion=mostrar_menu()
         if opcion == 1:
             interes_simple()
         elif opcion==2:
-            print("Elegiste interés compuesto")
+            interes_compuesto()
         elif opcion==3:
             print("Hasta luego.")
             break
